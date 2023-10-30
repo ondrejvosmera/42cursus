7ee0499060410f3809d38333fbe7e4a534207cce
ondrejvosmera <ondravosmera@seznam.cz>
2023-10-30 16:46:48 +0100

    ft_itoa

---
+++ 00_Libft/ft_itoa.c
@@ -0,0 +1,63 @@
+/*
+** ft_itoa
+** Allocatesand returns a string representing the integer
+** received as an argument. Negative numbers must be handled.
+*/
+
+#include "libft.h"
+
+int num_len(int n)
+{
+    int len;
+
+    len = 0;
+    if (n < 0)
+        len++;
+    else if (n == 0)
+        return (1);
+    while (n != 0)
+    {
+        n = n / 10;
+        len++;
+    }
+    return (len);
+}
+
+char *ft_itoa(int n)
+{
+    char        *str;
+    int         len;
+    long long   num;
+    
+    num = n;
+    len = num_len(n);
+    str = malloc(sizeof(char) * (len + 1));
+    if (str == NULL)
+        return (NULL);
+    if (num < 0)
+    {
+        num = num * (-1);
+        str[0] = '-';
+    }
+    else if (num == 0)
+        str[0] = '0';
+    str[len] = '\0'; 
+    while (num != 0)
+    {
+        str[--len] = num % 10 + '0';
+        num = num / 10;
+    }
+    return (str);
+}
+
+/* 
+#include <stdio.h>
+
+int main()
+{
+    int num = -42;
+    char *result = ft_itoa(num);
+    printf("Number: %d\n", num);
+    printf("After ft_itoa: %s\n", result);
+}
+ */
--- 00_Libft/libft.h
+++ 00_Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ondrejvosmera <ondrejvosmera@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 12:17:41 by ovosmera          #+#    #+#             */
-/*   Updated: 2023/10/30 12:46:46 by ondrejvosme      ###   ########.fr       */
+/*   Updated: 2023/10/30 13:36:34 by ondrejvosme      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,5 +53,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char    **ft_split(char const *s, char c);
+char    *ft_itoa(int n);
 
 #endif
