bd9a08b9ad9a2c31436c5b34b992d2196054e473
ondrejvosmera <ondravosmera@seznam.cz>
2023-11-15 14:49:59 +0100

    push to 42

---
+++ 01_ft_printf/ft_format_type.c
@@ -0,0 +1,9 @@
+#include "libftprintf.h"
+
+void    ft_format_type(char *format, va_list args, int *ptr_chars_printed)
+{
+    if (*format == 'c')
+        
+    else if (*format == 's')
+        
+}
--- 01_ft_printf/ft_printf.c
+++ 01_ft_printf/ft_printf.c
@@ -1,7 +1,20 @@
 #include "libftprintf.h"
 
-int ft_printf(const char *, ...)
+int ft_printf(const char *format, ...)
 {
     va_list args;
-    
+    int     chars_printed;
+    int     *ptr_chars_printed;
+
+    chars_printed = 0;
+    ptr_chars_printed = &chars_printed;
+    va_start(args, format);
+
+    while(*format != '\0')
+    {
+        if (format == '%')
+        {
+            format++;
+        }
+    }
 }
